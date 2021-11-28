cbdba222eabdf2da8138879bff74c609c80eae92
oneboyi <88018678+oneboyi@users.noreply.github.com>
2021-11-28 20:39:50 +0800

    Create 1000 A+B problem.cpp

---
+++ HDUOJ/1000-1999/1000 A+B problem.cpp
@@ -0,0 +1,14 @@
+/*
+ * @Author: oneboyi
+ * @Date: 2021-10-17 08:51:35
+ * @LastEditTime: 2021-11-28 20:39:13
+ * @LastEditors: oneboyi
+ */
+#include<iostream>
+using namespace std;
+int main(){
+	int a, b;
+	while(cin>>a>>b)
+		cout << a + b << endl;
+	return 0;
+}
