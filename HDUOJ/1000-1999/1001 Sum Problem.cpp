d20a8332b0c3ebd6b3be2e1bec8dbc0ab20323be
oneboyi <88018678+oneboyi@users.noreply.github.com>
2021-11-28 20:46:16 +0800

    Create 1001 Sum Problem.cpp

---
+++ HDUOJ/1000-1999/1001 Sum Problem.cpp
@@ -0,0 +1,15 @@
+/*
+ * @Author: oneboyi
+ * @Date: 2021-11-23 17:26:20
+ * @LastEditTime: 2021-11-28 20:44:55
+ * @LastEditors: oneboyi
+ */
+#include<iostream>
+using namespace std;
+int main(void)
+{
+	int n;
+	while(cin>>n)
+		cout << n * (n + 1) / 2<<endl<<endl;
+	return 0;
+}
